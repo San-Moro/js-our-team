b621251109fe8aaa590fc893b91f18aeda9f1a1e
San-Moro <valentinacortese.dsg@gmail.com>
2022-10-20 15:41:21 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+	
+let membersArray = [
+    {
+      name: "Wayne Barnett",
+      role: "Founder & CEO",
+      photo: "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+      name: "Angela Caroll",
+      role: "Chief Editor",
+      photo: "angela-caroll-chief-editor.jpg",
+    },
+    {
+      name: "Walter Gordon",
+      role: "Office Manager",
+      photo: "walter-gordon-office-manager.jpg",
+    },
+    {
+      name: "Angela Lopez",
+      role: "Social Media Manager",
+      photo: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+      name: "Scott Estrada",
+      role: "Developer",
+      photo: "scott-estrada-developer.jpg",
+    },
+    {
+      name: "Barbara Ramos",
+      role: "Graphic Designer",
+      photo: "barbara-ramos-graphic-designer.jpg",
+    }, 
+];
